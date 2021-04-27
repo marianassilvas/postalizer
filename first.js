02c92f1b9c41a5df4d391cf5975c328f1c47035f
ricardosacadura <ricardorochasacadura@gmail.com>
2021-04-27 15:33:51 +0100

    P5 teste 2

--- first.js
+++ first.js
@@ -1,12 +1,29 @@
+let pressed = true;
+let phrase;
+let words;
+
 function setup() {
 
-    createCanvas(200,200);
-    background(100);
-    textSize(20);
-    noStroke();
+  createCanvas(1000, 600);
 
-    let words = RiTa.tokenize("The elephant took a bite!")
-    for (let i=0; i < words.length; i++) {
-        text(words[i], 50, 50 + i*20);
-    }
-  }
+}
+
+function draw() {
+
+  background(0);
+  fill(255);
+  textSize(30);
+
+  phrase = "Hello, how are you today?";
+  words = RiTa.pos(phrase);
+
+  text(phrase, 50, 100, 1000, 30);
+  if (pressed) text(words, 50, 200, 30, 30);
+
+}
+
+function mousePressed() {
+
+  pressed = !pressed;
+
+}
