e061eb189e0f4f74ee42a4322aa90261d0443407
marianassilvas <crania.mariana@gmail.com>
2021-05-03 00:45:54 +0100

    navs_estrutura_0.1

--- teste.js
+++ teste.js
@@ -1,7 +1,10 @@
 function setup(){
-
+createCanvas(1300,600);
 }
 function draw(){
-    //ellipse(x,y,width,height);
-    ellipse(30,30,50,50);
+  background(100, 149, 237);
+  fill(255);
+  ellipse(mouseX, mouseY, 250, 250);
+  rect(150, 95, 200, 250);
+  line(mouseX, 0, mouseX, 100);
 }
