fa25a621a908af3e2e151d5af1c20b1b9df03b7d
marianassilvas <crania.mariana@gmail.com>
2021-05-22 00:40:31 +0100

    gavetas_terminadasanim

---
+++ create.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+
+  // ----------------create
+
+  $("#create1").on("click", function() {
+    let windowHeight2 = $(document).height();
+    //let topNavHeight = $(".topnavbar").height();
+    //console.log(windowHeight);
+
+    if ($(".animgaveta1").height() < 71) { //quando está fechado e há click
+
+      $(".animgaveta1").finish().animate({ //abre....
+        height: windowHeight2 * 1.75,
+
+      }, 500);
+
+      //e acontece:
+      $(".animgaveta1 .block").css("overflow", "visible");
+      $("#create").addClass("abertostyle");
+      $(".createcontainer").css("display", "flex");
+
+      $(".abertostyle").css("background-color", "white");
+      $("#titulocreate").css("color", "black");
+      $("#create svg > path").css("fill", "black");
+      $("#create svg").css("transform", "rotate(180deg)");
+
+      //e enquanto está aberto, quando se passa o rato na DIV..:
+      $("#create").hover(function() {
+       $("#titulocreate").css("color", "black");
+       $("#create svg > path").css("fill", "black");
+       $(".abertostyle").css("background-color", "white");
+
+        //sem o hover, volta ao normal...
+      }, function() {
+        $("#titulocreate").css("color", "black");
+        $("#create svg > path").css("fill", "black");
+        $(".abertostyle").css("background-color", "white");
+        //$("#create svg > path").css("fill", "black");
+      });
+
+
+      //e enquanto está aberto, quando se passa o rato na SETA..:
+      $("#create1 > svg").hover(function() {
+      $("#create svg > path").css("fill", "red");
+        //sem o hover, volta ao normal...
+      }, function() {
+      $("#create svg > path").css("fill", "black");
+      });
+
+
+      $(this).off('mouseenter'); //acaba quando se acaba de carregar
+
+
+    } else {
+      //quando está aberto e há click...
+      $(".animgaveta1").finish().animate({ //fecha
+        height: "100%",
+      }, 500);
+      //e acontece:
+      $("#create").removeClass("abertostyle");
+      $(".createcontainer").css("display", "none");
+      $("#create svg").css("transform", "rotate(0deg)");
+
+      //e enquanto está fechado, quando se passa o rato NA DIV..:
+      $("#create").hover(function() {
+        $("#create").css("background-color", "black");
+        $("#titulocreate").css("color", "white");
+        $("#create svg > path").css("fill", "white");
+
+        //sem o hover, volta ao normal...
+      }, function() {
+        $("#create").css("background-color", "white");
+        $("#titulocreate").css("fill", "black");
+        $("#create svg > path").css("fill", "black");
+      });
+
+      //e enquanto está fechado, quando se passa o rato na SETA..:
+      $("#create1 > svg").hover(function() {
+      $("#create svg > path").css("fill", "red");
+        //sem o hover, volta ao normal...
+      }, function() {
+      $("#create svg > path").css("fill", "white");
+      });
+
+    }
+    printF();
+  });
+
+  //---só quando clicka na seta
+  $("#create svg").on("click", function() {
+
+  });
+
+
+
+});//doc  finish
---
+++ loginregister.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+
+
+// ------------loginregister
+
+$("#loginregister1").on("click", function() {
+  let windowHeight = $(document).height();
+
+  if ($(".animgaveta2").height() < 71) { //quando está fechado e há click
+
+    $(".animgaveta2").finish().animate({ //abre....
+      height: windowHeight * 1.75,
+
+    }, 500);
+
+    //e acontece:
+    $(".animgaveta2 .block").css("overflow", "visible");
+    $("#loginregister").addClass("abertostyle");
+    $(".loginregistercontainer").css("display", "flex");
+
+    $(".abertostyle").css("background-color", "white");
+    $("#titulologinregister").css("color", "black");
+    $("#loginregister svg > path").css("fill", "black");
+    $("#loginregister svg").css("transform", "rotate(180deg)");
+
+    //e enquanto está aberto, quando se passa o rato na DIV..:
+    $("#loginregister").hover(function() {
+     $("#titulologinregister").css("color", "black");
+     $(".abertostyle").css("background-color", "white");
+     $("#loginregister svg > path").css("fill", "black");
+
+      //sem o hover, volta ao normal...
+    }, function() {
+      $("#titulologinregister").css("color", "black");
+      $(".abertostyle").css("background-color", "white");
+      $("#loginregister svg > path").css("fill", "black");
+
+    });
+
+    //e enquanto está aberto, quando se passa o rato na SETA..:
+    $("#loginregister1 > svg").hover(function() {
+    $("#loginregister1 svg > path").css("fill", "red");
+      //sem o hover, volta ao normal...
+    }, function() {
+    $("#loginregister svg > path").css("fill", "black");
+    });
+
+
+    $(this).off('mouseenter'); //acaba quando se acaba de carregar
+
+
+  } else { //quando está aberto e há click...
+    $(".animgaveta2").finish().animate({ //fecha
+      height: "100%",
+    }, 500);
+
+    //e acontece:
+    $("#loginregister").removeClass("abertostyle");
+    $(".loginregistercontainer").css("display", "none");
+    $("#loginregister svg").css("transform", "rotate(0deg)");
+
+    //e enquanto está fechado, quando se passa o rato NA DIV..:
+    $("#loginregister").hover(function() {
+      $("#loginregister").css("background-color", "black");
+      $("#titulologinregister").css("color", "white");
+      $("#loginregister svg > path").css("fill", "white");
+
+      //sem o hover, volta ao normal...
+    }, function() {
+      $("#loginregister").css("background-color", "white");
+      $("#titulologinregister").css("fill", "black");
+      $("#loginregister svg > path").css("fill", "black");
+    });
+
+    //e enquanto está fechado, quando se passa o rato na SETA..:
+    $("#loginregister > svg").hover(function() {
+    $("#loginregister svg > path").css("fill", "red");
+      //sem o hover, volta ao normal...
+    }, function() {
+    $("#loginregister svg > path").css("fill", "white");
+    });
+
+  }
+
+  printF();
+});
+
+});
